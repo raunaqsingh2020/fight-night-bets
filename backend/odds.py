00a9f933cbdcac6643d78d68528d63a47884b40c
EC2 Default User <ec2-user@ip-172-31-35-219.ec2.internal>
2024-04-25 18:11:18 +0000

    fix -100

--- backend/odds.py
+++ backend/odds.py
@@ -94,8 +94,17 @@ def calculate_american_odds(event: int, max_bet: float = 10.0) -> float:
 
     if outcome_b_odds > 0 and outcome_b_odds < 100:
         outcome_b_odds = -1.0 * (100.0 / outcome_b_odds) * 100.0
+    
+    outcome_a_odds = int(outcome_a_odds)
+    outcome_b_odds = int(outcome_b_odds)
 
-    return int(outcome_a_odds), int(outcome_b_odds)
+    if outcome_a_odds == -100:
+        outcome_a_odds = 100
+    
+    if outcome_b_odds == -100:
+        outcome_b_odds = 100
+
+    return outcome_a_odds, outcome_b_odds
 
 
 def check_amount_in_pools(event: int) -> tuple[int]:
